5769469525ebaaf032350d5413478012708cdfc6
Mars-37 <master_sbo@mail.ru>
2024-05-01 00:38:13 +0300

    spread CreateRandFile

--- FileManager.h
+++ FileManager.h
@@ -12,38 +12,7 @@
 class FileManager
 {
 public:
-	static void CreateRandFile(const std::string& filename)
-	{
-		// file variable
-		std::fstream fs;
-
-		// random number generator initialization
-		srand(static_cast<unsigned int>(time(nullptr)));
-
-		// selection range constant
-		const int low = -1'000;
-		const int high = 4'000;
-
-		// open the file for writing to
-		fs.open(filename, std::fstream::out | std::ofstream::trunc);
-
-		// file opening check
-		if (!fs.is_open()) {
-			throw std::runtime_error("Failed to open file for writing");
-		}
-
-		std::cout << "File " << filename << " created\n";
-
-		// fill the file with numbers		
-		for (int i = 0; i < LINE_IN_FILE; i++) {
-			fs << (low + rand() % high) << std::endl;
-		}
-
-		// close file
-		fs.close();
-
-		std::cout << "File " << filename << " is full\n";
-	}
+	static void CreateRandFile(const std::string& filename);
 	static void MergeToFile(const int* arr1, const int* arr2, int elements1, int elements2)
 	{
 		// file variable
