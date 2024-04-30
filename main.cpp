62ca54ab281c5e990b62e856cf1ff9d935bfa875
Mars-37 <master_sbo@mail.ru>
2024-04-30 21:54:43 +0300

    optimization Sorting and SortingManager classes are merged

--- main.cpp
+++ main.cpp
@@ -20,74 +20,6 @@
 #define TMP_BLOCK		5'000'000
 
 
-class Sorting
-{
-public:
-	template <typename T>
-	static void MergeSort(T* arr, int low, int high)
-	{
-		// algorithm condition
-		if (low < high) {
-			// find the middle of the array
-			int mid = low + (high - low) / 2;
-
-			// recursively sort left half of the array
-			MergeSort(arr, low, mid);
-
-			// recursively sort right half of the array
-			MergeSort(arr, mid + 1, high);
-
-			// merge sorted halves of the array
-			Merge(arr, low, mid, high);
-		}
-	}
-
-private:
-	template <typename T>
-	static void Merge(T* arr, int low, int mid, int high)
-	{
-		// calculate dimensions of the halves
-		int n1 = mid - low + 1;
-		int n2 = high - mid;
-
-		// create temporary arrays to store halves
-		T* left = new T[n1];
-		T* right = new T[n2];
-
-		// copy data to temporary arrays
-		for (int i = 0; i < n1; i++)
-			left[i] = arr[low + i];
-		for (int j = 0; j < n2; j++)
-			right[j] = arr[mid + 1 + j];
-
-		// merge temporary arrays back into the main array
-		int i = 0, j = 0, k = low;
-		while (i < n1 && j < n2) {
-			if (left[i] <= right[j]) {
-				arr[k++] = left[i++];
-			}
-			else {
-				arr[k++] = right[j++];
-			}
-		}
-
-		// copy the remaining elements from the left array
-		while (i < n1) {
-			arr[k++] = left[i++];
-		}
-
-		// copy the remaining elements from the right array
-		while (j < n2) {
-			arr[k++] = right[j++];
-		}
-
-		// free the allocated memory
-		delete[] left;
-		delete[] right;
-	}
-};
-
-
 class FileManager
 {
 public:
@@ -296,6 +228,24 @@ public:
 class SortingManager
 {
 public:
+	template <typename T>
+	static void MergeSort(T* arr, int low, int high)
+	{
+		// algorithm condition
+		if (low < high) {
+			// find the middle of the array
+			int mid = low + (high - low) / 2;
+
+			// recursively sort left half of the array
+			MergeSort(arr, low, mid);
+
+			// recursively sort right half of the array
+			MergeSort(arr, mid + 1, high);
+
+			// merge sorted halves of the array
+			Merge(arr, low, mid, high);
+		}
+	}
 	static void RunSort(const std::string& filename, std::string& resultfilename)
 	{
 		// static process indicator variable
@@ -332,8 +282,8 @@ public:
 			std::cout << " string processing = " << processed << std::endl;
 
 			// implementation of sorting by merge
-			Sorting::MergeSort(part1.get(), 0, size1 - 1);
-			Sorting::MergeSort(part2.get(), 0, size2 - 1);
+			MergeSort(part1.get(), 0, size1 - 1);
+			MergeSort(part2.get(), 0, size2 - 1);
 
 			// merge two sorted arrays
 			FileManager::MergeToFile(part1.get(), part2.get(), size1, size2);
@@ -341,6 +291,50 @@ public:
 		}
 
 		fs.close();
+	}
+
+private:
+	template <typename T>
+	static void Merge(T* arr, int low, int mid, int high)
+	{
+		// calculate dimensions of the halves
+		int n1 = mid - low + 1;
+		int n2 = high - mid;
+
+		// create temporary arrays to store halves
+		T* left = new T[n1];
+		T* right = new T[n2];
+
+		// copy data to temporary arrays
+		for (int i = 0; i < n1; i++)
+			left[i] = arr[low + i];
+		for (int j = 0; j < n2; j++)
+			right[j] = arr[mid + 1 + j];
+
+		// merge temporary arrays back into the main array
+		int i = 0, j = 0, k = low;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k++] = left[i++];
+			}
+			else {
+				arr[k++] = right[j++];
+			}
+		}
+
+		// copy the remaining elements from the left array
+		while (i < n1) {
+			arr[k++] = left[i++];
+		}
+
+		// copy the remaining elements from the right array
+		while (j < n2) {
+			arr[k++] = right[j++];
+		}
+
+		// free the allocated memory
+		delete[] left;
+		delete[] right;
 	}
 };
 
