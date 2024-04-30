fb27be16f7ce1cd31de06a273b889229d2aadb5a
Mars-37 <master_sbo@mail.ru>
2024-05-01 00:55:22 +0300

    spread MergeFiles

--- FileManager.cpp
+++ FileManager.cpp
@@ -88,5 +88,65 @@ void FileManager::MergeToFile(const int* arr1, const int* arr2, int elements1, i
 
 	// close file
 	temp.close();
+}
+void FileManager::MergeFiles(const std::string& resultfilename)
+{
+	// files variables
+	std::fstream res;
+	std::fstream temp1;
+	std::fstream temp2;
 
+	// temporary file name
+	const std::string& tmp1 = "tmp1.txt";
+	const std::string& tmp2 = "tmp2.txt";
+
+	// files opening
+	temp1.open(tmp1, std::fstream::in);
+	res.open(resultfilename, std::fstream::in);
+	temp2.open(tmp2, std::fstream::out | std::ofstream::trunc);
+
+	// if at least one file is not open
+	if (!temp1.is_open() || !temp2.is_open() || !res.is_open()) {
+		return;
+	}
+
+	// temp values
+	int temp1_value;
+	int res_value;
+
+
+	temp1 >> temp1_value;
+	res >> res_value;
+	while (!temp1.eof() && !res.eof()) {
+		if (temp1_value <= res_value) {
+			temp2 << temp1_value << std::endl;
+			temp1 >> temp1_value;
+		}
+		else {
+			temp2 << res_value << std::endl;
+			res >> res_value;
+		}
+	}
+
+	// files merge
+	while (!res.eof()) {
+		temp2 << res_value << std::endl;
+		res >> res_value;
+	}
+
+	while (!temp1.eof()) {
+		temp2 << temp1_value << std::endl;
+		temp1 >> temp1_value;
+	}
+
+	// files close
+	temp1.close();
+	temp2.close();
+	res.close();
+
+	// copies the contents of the temporary file to the resulting file
+	if (!std::filesystem::copy_file("tmp2.txt", resultfilename,
+		std::filesystem::copy_options::overwrite_existing)) {
+		return;
+	}
 }
--- FileManager.h
+++ FileManager.h
@@ -14,67 +14,7 @@ class FileManager
 public:
 	static void CreateRandFile(const std::string& filename);
 	static void MergeToFile(const int* arr1, const int* arr2, int elements1, int elements2);
-	static void MergeFiles(const std::string& resultfilename)
-	{
-		// files variables
-		std::fstream res;
-		std::fstream temp1;
-		std::fstream temp2;
-
-		// temporary file name
-		const std::string& tmp1 = "tmp1.txt";
-		const std::string& tmp2 = "tmp2.txt";
-
-		// files opening
-		temp1.open(tmp1, std::fstream::in);
-		res.open(resultfilename, std::fstream::in);
-		temp2.open(tmp2, std::fstream::out | std::ofstream::trunc);
-
-		// if at least one file is not open
-		if (!temp1.is_open() || !temp2.is_open() || !res.is_open()) {
-			return;
-		}
-
-		// temp values
-		int temp1_value;
-		int res_value;
-
-
-		temp1 >> temp1_value;
-		res >> res_value;
-		while (!temp1.eof() && !res.eof()) {
-			if (temp1_value <= res_value) {
-				temp2 << temp1_value << std::endl;
-				temp1 >> temp1_value;
-			}
-			else {
-				temp2 << res_value << std::endl;
-				res >> res_value;
-			}
-		}
-
-		// files merge
-		while (!res.eof()) {
-			temp2 << res_value << std::endl;
-			res >> res_value;
-		}
-
-		while (!temp1.eof()) {
-			temp2 << temp1_value << std::endl;
-			temp1 >> temp1_value;
-		}
-
-		// files close
-		temp1.close();
-		temp2.close();
-		res.close();
-
-		// copies the contents of the temporary file to the resulting file
-		if (!std::filesystem::copy_file("tmp2.txt", resultfilename,
-			std::filesystem::copy_options::overwrite_existing)) {
-			return;
-		}
-	}
+	static void MergeFiles(const std::string& resultfilename);
 	static int ReadTempBlock(std::fstream& fs, std::unique_ptr<int[]>& arr)
 	{
 		// allocate memory for an array of size TMP_BLOCK
